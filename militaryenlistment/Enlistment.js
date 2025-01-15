b3af34dc56778f5e3e54a76134799dd0cf6fd9e9
Beatriz Cesconetto <beatriz.cesconetto@compasso.com.br>
2025-01-15 14:14:00 -0300

    fix olderthen18 and enlistment

--- militaryenlistment/Enlistment.js
+++ militaryenlistment/Enlistment.js
@@ -1,9 +1,12 @@
+function enlistment() {
 let userAge = prompt("How old are you: ");
 let userGender = prompt("Type M for male or F for female:  ");
-if (userAge >= 18 && userGender == "M") {
-    console.log("You need to enlist in the military");
-} else if(userAge <= 18 && userGender == "M"){
-    console.log("You are younger than 18 you can wait a few years");
-}else{
-    console.log("You don't need to enlist in the military");
+
+    if (userAge >= 18 && userGender == "M") {
+        console.log("You need to enlist in the military");
+    } else if(userAge <= 18 && userGender == "M"){
+        console.log("You are younger than 18 you can wait a few years");
+    }else{
+        console.log("You don't need to enlist in the military");
+    }
 }
--- olderthen18/OlderThen18.js
+++ olderthen18/OlderThen18.js
@@ -1,6 +1,8 @@
+function checkAge() {
 let userAge = prompt("How old are you: ");
-if (userAge >= 18) {
-    console.log("You are older than 18");
-} else {
-    console.log("You are younger than 18");
+    if (userAge >= 18) {
+        console.log("You are older than 18");
+    } else {
+        console.log("You are younger than 18");
+    }
 }
