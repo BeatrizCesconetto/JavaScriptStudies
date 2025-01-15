6a47f8e8f6a431dc3afc586188065b41d5dee1b8
Beatriz Cesconetto <beatriz.cesconetto@compasso.com.br>
2025-01-15 13:41:20 -0300

    enlistment and older than 18

---
+++ militaryenlistment/Enlistment.js
@@ -0,0 +1,9 @@
+let userAge = prompt("How old are you: ");
+let userGender = prompt("Type M for male or F for female:  ");
+if (userAge >= 18 && userGender == "M") {
+    console.log("You need to enlist in the military");
+} else if(userAge <= 18 && userGender == "M"){
+    console.log("You are younger than 18 you can wait a few years");
+}else{
+    console.log("You don't need to enlist in the military");
+}
---
+++ olderthen18/OlderThen18.js
@@ -0,0 +1,6 @@
+let userAge = prompt("How old are you: ");
+if (userAge >= 18) {
+    console.log("You are older than 18");
+} else {
+    console.log("You are younger than 18");
+}
